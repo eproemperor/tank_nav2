28bba6335f767692ce590540c2941b3f47afbd30
eproemperor <eproemperor@outlook.com>
2026-06-06 15:11:45 +0800

    6.6

---
+++ robot_behavior_tree/include/gobase_node.h
@@ -0,0 +1,16 @@
+#ifndef GOBASE_NODE.H
+#define GOBASE_NODE.H
+
+#include <mutex>
+#include <rclcpp/rclcpp.hpp>
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+namespace gobase{
+    class Gobase : public BT::SyncActionNode{
+        
+    };
+}
+
+#endif
