4690c4e8be1d4c2a7e87962b610a7ed9e7a55039
eproemperor <eproemperor@outlook.com>
2026-06-08 22:15:58 +0800

    6.8

--- robot_behavior_tree/include/gobase_node.h
+++ robot_behavior_tree/include/gobase_node.h
@@ -1,3 +1,9 @@
+/*输入：目标位置
+    流程：前往下一个敌人（每”UPDATEEMERYPOSITION“时间更新目标点）-->检测血量&子弹数量-->（前往补给区）-->寻找第二个敌人（每”UPDATEEMERYPOSITION“时间更新目标点）
+        -->前往传送点-->前往密 码发区-->前往传送点-->前往基地
+        只给点，路径由规划器决定，移动由规划器控制
+*/
+
 #ifndef GOBASE_NODE.H
 #define GOBASE_NODE .H
 
@@ -9,6 +15,28 @@
 
 namespace gobase
 {
+    enum TargetType : uint8_t
+    {
+        STAR = 0,        // 星星/星标
+        BASE = 1,        // 基地
+        ENEMY_BASE = 2,  // 敌方基地
+        PURPLEENTRY = 3, // 紫色入口
+        GREENENTRY = 4,  // 绿色入口
+        SENTRY = 5,      // 哨兵
+        ENEMY = 6,       // 敌方单位
+        PURPLEEXIT = 7,  // 紫色出口
+        GREENEXIT = 8,   // 绿色出口
+    };
+
+    struct TypeMode
+    {
+        TargetType name;
+        bool is_exist = true;
+        double p_x;
+        double p_y;
+        double p_z = 0.0;
+    };
+
     class Gobase : public BT::SyncActionNode
     {
     public:
@@ -17,8 +45,16 @@ namespace gobase
         BT::NodeStatus tick() override;
 
     private:
+        TypeMode star;
+        TypeMode base;
+        TypeMode enemy_base;
+        TypeMode purpleentry;
+        TypeMode greenentry;
+        TypeMode sentry;
+        TypeMode enemy;
+        TypeMode purpleexit;
+        TypeMode greenexit;
         rclcpp::Node::SharedPtr node_;
-        rclcpp::CallbackGroup::SharedPtr callback_group_;
     };
 }
 
