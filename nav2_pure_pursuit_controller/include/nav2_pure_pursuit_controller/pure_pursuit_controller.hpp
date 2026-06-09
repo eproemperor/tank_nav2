b7a6613a42ad1087f4c5e46902da68fc9157bd18
eproemperor <eproemperor@outlook.com>
2026-06-09 13:09:31 +0800

    6.9

--- nav2_pure_pursuit_controller/include/nav2_pure_pursuit_controller/pure_pursuit_controller.hpp
+++ nav2_pure_pursuit_controller/include/nav2_pure_pursuit_controller/pure_pursuit_controller.hpp
@@ -3,6 +3,8 @@
  *
  *  Author(s): Shrijit Singh <shrijitsingh99@gmail.com>
  *
+ * Nav2 (Navigation2) 框架中的纯追踪控制器 的头文件。
+ * 它实现了一个路径跟踪算法，用于让机器人沿着规划好的全局路径运动
  */
 
 #ifndef NAV2_PURE_PURSUIT_CONTROLLER__PURE_PURSUIT_CONTROLLER_HPP_
@@ -20,57 +22,55 @@
 namespace nav2_pure_pursuit_controller
 {
 
-class PurePursuitController : public nav2_core::Controller
-{
-public:
-  PurePursuitController() = default;
-  ~PurePursuitController() override = default;
+  class PurePursuitController : public nav2_core::Controller
+  {
+  public:
+    PurePursuitController() = default;
+    ~PurePursuitController() override = default;
 
-  void configure(
-    const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
-    std::string name, const std::shared_ptr<tf2_ros::Buffer> tf,
-    const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
+    void configure(
+        const rclcpp_lifecycle::LifecycleNode::WeakPtr &parent,
+        std::string name, const std::shared_ptr<tf2_ros::Buffer> tf,
+        const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
 
+    void cleanup() override;
+    void activate() override;
+    void deactivate() override;
+    void setSpeedLimit(const double &speed_limit, const bool &percentage) override;
 
-  void cleanup() override;
-  void activate() override;
-  void deactivate() override;
-  void setSpeedLimit(const double & speed_limit, const bool & percentage) override;
+    geometry_msgs::msg::TwistStamped computeVelocityCommands(
+        const geometry_msgs::msg::PoseStamped &pose,
+        const geometry_msgs::msg::Twist &velocity,
+        nav2_core::GoalChecker *goal_checker) override;
 
-  geometry_msgs::msg::TwistStamped computeVelocityCommands(
-    const geometry_msgs::msg::PoseStamped & pose,
-    const geometry_msgs::msg::Twist & velocity,
-    nav2_core::GoalChecker * goal_checker) override;
+    void setPlan(const nav_msgs::msg::Path &path) override;
 
-  void setPlan(const nav_msgs::msg::Path & path) override;
+  protected:
+    nav_msgs::msg::Path transformGlobalPlan(const geometry_msgs::msg::PoseStamped &pose);
 
-protected:
-  nav_msgs::msg::Path transformGlobalPlan(const geometry_msgs::msg::PoseStamped & pose);
+    bool transformPose(
+        const std::shared_ptr<tf2_ros::Buffer> tf,
+        const std::string frame,
+        const geometry_msgs::msg::PoseStamped &in_pose,
+        geometry_msgs::msg::PoseStamped &out_pose,
+        const rclcpp::Duration &transform_tolerance) const;
 
-  bool transformPose(
-    const std::shared_ptr<tf2_ros::Buffer> tf,
-    const std::string frame,
-    const geometry_msgs::msg::PoseStamped & in_pose,
-    geometry_msgs::msg::PoseStamped & out_pose,
-    const rclcpp::Duration & transform_tolerance
-  ) const;
+    rclcpp_lifecycle::LifecycleNode::WeakPtr node_;
+    std::shared_ptr<tf2_ros::Buffer> tf_;
+    std::string plugin_name_;
+    std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_;
+    rclcpp::Logger logger_{rclcpp::get_logger("PurePursuitController")};
+    rclcpp::Clock::SharedPtr clock_;
 
-  rclcpp_lifecycle::LifecycleNode::WeakPtr node_;
-  std::shared_ptr<tf2_ros::Buffer> tf_;
-  std::string plugin_name_;
-  std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_;
-  rclcpp::Logger logger_ {rclcpp::get_logger("PurePursuitController")};
-  rclcpp::Clock::SharedPtr clock_;
+    double desired_linear_vel_;
+    double lookahead_dist_;
+    double max_angular_vel_;
+    rclcpp::Duration transform_tolerance_{0, 0};
 
-  double desired_linear_vel_;
-  double lookahead_dist_;
-  double max_angular_vel_;
-  rclcpp::Duration transform_tolerance_ {0, 0};
+    nav_msgs::msg::Path global_plan_;
+    std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>> global_pub_;
+  };
 
-  nav_msgs::msg::Path global_plan_;
-  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>> global_pub_;
-};
+} // namespace nav2_pure_pursuit_controller
 
-}  // namespace nav2_pure_pursuit_controller
-
-#endif  // NAV2_PURE_PURSUIT_CONTROLLER__PURE_PURSUIT_CONTROLLER_HPP_
+#endif // NAV2_PURE_PURSUIT_CONTROLLER__PURE_PURSUIT_CONTROLLER_HPP_
--- robot_bt_navigator/include/robot_bt_navigator/bt_navigator.hpp
+++ robot_bt_navigator/include/robot_bt_navigator/bt_navigator.hpp
@@ -31,79 +31,79 @@
 namespace nav2_bt_navigator
 {
 
-/**
- * @class nav2_bt_navigator::BtNavigator
- * @brief An action server that uses behavior tree for navigating a robot to its
- * goal position.
- */
-class BtNavigator : public nav2_util::LifecycleNode
-{
-public:
   /**
-   * @brief A constructor for nav2_bt_navigator::BtNavigator class
-   * @param options Additional options to control creation of the node.
+   * @class nav2_bt_navigator::BtNavigator
+   * @brief An action server that uses behavior tree for navigating a robot to its
+   * goal position.
    */
-  explicit BtNavigator(rclcpp::NodeOptions options = rclcpp::NodeOptions());
-  /**
-   * @brief A destructor for nav2_bt_navigator::BtNavigator class
-   */
-  ~BtNavigator();
+  class BtNavigator : public nav2_util::LifecycleNode
+  {
+  public:
+    /**
+     * @brief A constructor for nav2_bt_navigator::BtNavigator class
+     * @param options Additional options to control creation of the node.
+     */
+    explicit BtNavigator(rclcpp::NodeOptions options = rclcpp::NodeOptions());
+    /**
+     * @brief A destructor for nav2_bt_navigator::BtNavigator class
+     */
+    ~BtNavigator();
 
-protected:
-  /**
-   * @brief Configures member variables
-   *
-   * Initializes action server for "NavigationToPose"; subscription to
-   * "goal_sub"; and builds behavior tree from xml file.
-   * @param state Reference to LifeCycle node state
-   * @return SUCCESS or FAILURE
-   */
-  nav2_util::CallbackReturn on_configure(const rclcpp_lifecycle::State & state) override;
-  /**
-   * @brief Activates action server
-   * @param state Reference to LifeCycle node state
-   * @return SUCCESS or FAILURE
-   */
-  nav2_util::CallbackReturn on_activate(const rclcpp_lifecycle::State & state) override;
-  /**
-   * @brief Deactivates action server
-   * @param state Reference to LifeCycle node state
-   * @return SUCCESS or FAILURE
-   */
-  nav2_util::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & state) override;
-  /**
-   * @brief Resets member variables
-   * @param state Reference to LifeCycle node state
-   * @return SUCCESS or FAILURE
-   */
-  nav2_util::CallbackReturn on_cleanup(const rclcpp_lifecycle::State & state) override;
-  /**
-   * @brief Called when in shutdown state
-   * @param state Reference to LifeCycle node state
-   * @return SUCCESS or FAILURE
-   */
-  nav2_util::CallbackReturn on_shutdown(const rclcpp_lifecycle::State & state) override;
+  protected:
+    /**
+     * @brief Configures member variables
+     *
+     * Initializes action server for "NavigationToPose"; subscription to
+     * "goal_sub"; and builds behavior tree from xml file.
+     * @param state Reference to LifeCycle node state
+     * @return SUCCESS or FAILURE
+     */
+    nav2_util::CallbackReturn on_configure(const rclcpp_lifecycle::State &state) override;
+    /**
+     * @brief Activates action server
+     * @param state Reference to LifeCycle node state
+     * @return SUCCESS or FAILURE
+     */
+    nav2_util::CallbackReturn on_activate(const rclcpp_lifecycle::State &state) override;
+    /**
+     * @brief Deactivates action server
+     * @param state Reference to LifeCycle node state
+     * @return SUCCESS or FAILURE
+     */
+    nav2_util::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &state) override;
+    /**
+     * @brief Resets member variables
+     * @param state Reference to LifeCycle node state
+     * @return SUCCESS or FAILURE
+     */
+    nav2_util::CallbackReturn on_cleanup(const rclcpp_lifecycle::State &state) override;
+    /**
+     * @brief Called when in shutdown state
+     * @param state Reference to LifeCycle node state
+     * @return SUCCESS or FAILURE
+     */
+    nav2_util::CallbackReturn on_shutdown(const rclcpp_lifecycle::State &state) override;
 
-  // To handle all the BT related execution
-  std::unique_ptr<nav2_bt_navigator::Navigator<nav2_msgs::action::NavigateToPose>> pose_navigator_;
-  std::unique_ptr<nav2_bt_navigator::Navigator<nav2_msgs::action::NavigateThroughPoses>>
-  poses_navigator_;
-  nav2_bt_navigator::NavigatorMuxer plugin_muxer_;
+    // To handle all the BT related execution
+    std::unique_ptr<nav2_bt_navigator::Navigator<nav2_msgs::action::NavigateToPose>> pose_navigator_;
+    std::unique_ptr<nav2_bt_navigator::Navigator<nav2_msgs::action::NavigateThroughPoses>>
+        poses_navigator_;
+    nav2_bt_navigator::NavigatorMuxer plugin_muxer_;
 
-  // Odometry smoother object
-  std::shared_ptr<nav2_util::OdomSmoother> odom_smoother_;
+    // Odometry smoother object
+    std::shared_ptr<nav2_util::OdomSmoother> odom_smoother_;
 
-  // Metrics for feedback
-  std::string robot_frame_;
-  std::string global_frame_;
-  double transform_tolerance_;
-  std::string odom_topic_;
+    // Metrics for feedback
+    std::string robot_frame_;
+    std::string global_frame_;
+    double transform_tolerance_;
+    std::string odom_topic_;
 
-  // Spinning transform that can be used by the BT nodes
-  std::shared_ptr<tf2_ros::Buffer> tf_;
-  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-};
+    // Spinning transform that can be used by the BT nodes
+    std::shared_ptr<tf2_ros::Buffer> tf_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  };
 
-}  // namespace nav2_bt_navigator
+} // namespace nav2_bt_navigator
 
-#endif  // NAV2_BT_NAVIGATOR__BT_NAVIGATOR_HPP_
+#endif // NAV2_BT_NAVIGATOR__BT_NAVIGATOR_HPP_
